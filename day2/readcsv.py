f75acb33b5d5deb5a8b43e976ff0eb3c2fbd1310
yudantoanas <youdant@gmail.com>
2024-12-02 17:40:44 +0700

    update

--- day2/readcsv.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-
-with open('/home/raka/data.csv') as f:
-	myreader=csv.DictReader(f)
-	headers=next(myreader)
-	for row in myreader:
-		print(row['name'])
--- day3/1. databases/querydf.py
+++
@@ -1,8 +0,0 @@
-import psycopg2 as db
-import pandas as pd
-conn_string="dbname='postgres' host='localhost' user='postgres' password='rinintha'"
-conn=db.connect(conn_string)
-df=pd.read_sql("select * from people", conn)
-print(df.head())
-print(df['city'].value_counts())
-df.to_json(orient='records')
